175cd0a15ad593467f4e944a5327038c12742c66
rahmatiki <rahmatriyadiiki@gmail.com>
2021-05-11 10:19:37 +0700

    Change Condition using Heroku

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,3 +1,3 @@
 export default {
-    api: process.env.NODE_ENV == "development" ? process.env.REACT_APP_DEV_API_URL : process.env.REACT_APP_HEROKU_API_URL
+    api: window.location.protocol == "http:" ? process.env.REACT_APP_DEV_API_URL : process.env.REACT_APP_HEROKU_API_URL
 }
